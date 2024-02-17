c18ea7d50b5cdba7b81adade07725c63f31a46cb
Umang Raj <jackerumang@gmail.com>
2024-02-17 15:00:56 +0530

    All

---
+++ 2-BMICalculator/chaiaurcode.js
@@ -0,0 +1,22 @@
+//since it is a form so we will use submit event
+const form = document.querySelector('form');
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `please give a valid height ${height}`;
+  } else if (weight === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `please give a valid height ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+  }
+  if (results < 18.6) {
+    results.innerHTML += 'underweight';
+  }
+});
